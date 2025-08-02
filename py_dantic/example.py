612a04f591e398b68a0db1079522eda755f92f7f
Shehzail Abbas <abbasshehzail123@gmail.com>
2025-08-02 17:23:32 +0530

    initial commit

--- py_dantic/example.py
+++ py_dantic/example.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel,EmailStr,AnyUrl,Field,field_validator
+from pydantic import BaseModel,EmailStr,AnyUrl,Field,field_validator,computed_field
 from typing import List,Dict,Optional,Annotated
 class patient(BaseModel):
  
     name:str
     age: Annotated[int,Field(title='enter the age',description='enter the age of the patient it should be between 18 and 60',default=18)]
     weight: Annotated[float,Field(gt=0,strict=True)]
+    height: Annotated[float,Field(gt=0)]
     email: EmailStr
     url: AnyUrl
     married:bool=False
@@ -32,16 +33,22 @@ class patient(BaseModel):
             raise ValueError("age not in acceptable range")
         return value
     
+    @computed_field
+    @property
+    def bmi(self)->float:
+        return round(self.weight/(self.height**2),2)
+
 def insert_patient_data(Patient: patient):
     print(Patient.name)
     print(Patient.age)
     print(Patient.allergies)
     print(Patient.married)
+    print(Patient.bmi)
     print('inserted')
 
-patient_info={'name': 'shehzail','age':34,'weight':64.3, 
+patient_info={'name': 'shehzail','age':34,'weight':53.3, 
               'contact_details':{'email':'abbasshehzail123@gmail.com','phoneNo':'34234343'},
-              'email':'abbasshehzail123@hdfc.com','url':"https://hello.com"}
+              'email':'abbasshehzail123@hdfc.com','url':"https://hello.com",'height':1.7}
 Patient=patient(**patient_info)
 insert_patient_data(Patient)
 
